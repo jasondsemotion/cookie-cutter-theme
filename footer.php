d264e7af6909ad7d84360699a8f487648b13ec08
Jason <jason@dsemotion.com>
2019-03-22 17:17:19 +0000

    footer update

--- footer.php
+++ footer.php
@@ -17,17 +17,32 @@
     <div class="main-container">
         <div class="grid-x grid-margin-x">
             <div class="cell small-12 medium-12 large-8">
-                <div class="title"><h2>Contacts</h2></div>
-                <div class="logo">
-                    <img src="<?= $footer_logo['url'] ?>" alt="">
+
+				<div class="grid-x grid-margin-xx">
+					<div class="cell small-12">
+		                <div class="title"><h2>Contacts</h2></div>
+	                </div>
+				</div>
+
+                <div class="grid-x grid-margin-x footer-detail">
+	                <div class="cell small-12 medium-12 large-4 logo-col">
+		                <div class="logo">
+		                    <img src="<?= $footer_logo['url'] ?>" alt="">
+		                </div>
+	                </div>
+	                <?php if( have_rows('contact') ): while ( have_rows('contact') ) : the_row(); ?>
+	                <div class="cell small-12 medium-6 large-4">
+		                <div class="contact">
+		                 <div class="name"><?= get_sub_field('name'); ?></div>
+		                 <a href="mailto:<?= get_sub_field('email'); ?>"><?= get_sub_field('email'); ?></a><br>
+						 <a href="tel:<?= get_sub_field('contact_number'); ?>"><?= get_sub_field('contact_number'); ?></a>
+		                </div>
+	                </div>
+	                 <?php endwhile; else : endif; ?>
                 </div>
-                <?php if( have_rows('contact') ): while ( have_rows('contact') ) : the_row(); ?>
-                <div class="contact">
-                    <div class="name"><?= get_sub_field('name'); ?></div>
-                    <a href="mailto:<?= get_sub_field('email'); ?>"><?= get_sub_field('email'); ?></a><br>
-                    <a href="tel:<?= get_sub_field('contact_number'); ?>"><?= get_sub_field('contact_number'); ?></a>
-                </div>
-                <?php endwhile; else : endif; ?>
+
+
+
             </div>
             <div class="cell small-12 medium-12 large-4">
 	            <div class="col-txt">
