5d0232f9db1e1458504a4c969b69059c24345f86
Jason <jason@dsemotion.com>
2019-04-04 09:12:30 +0100

    fix

--- template-parts/embed/block.php
+++ template-parts/embed/block.php
@@ -13,24 +13,28 @@ $id = get_sub_field('section_id');
 
 <section class="page-section main-container emebed-container" id="<?php echo $id; ?>">
     <div class="grid-x grid-margin-x">
-	    <?php if($title) { ?>
-			<div class="cell small-12 large-6">
-				<div data-aos="fade-left" data-aos-delay="800" class="title">
-			                <h2><?= $title ?></h2>
-			    </div>
-			</div>
-		<?php } ?>
-		<?php if($embed_content) { ?>
-			<div class="cell small-12 large-6">
-				<div data-aos="fade-left" data-aos-delay="800" class="">
-			          <?= $embed_content ?>
-			    </div>
-			</div>
-		<?php } ?>
-        <div class="cell large-12">
-	        <div class="embed-container">
-            <?php echo get_sub_field('embed_code'); ?>
+
+		    <?php if($title) { ?>
+				<div class="cell small-12 large-6">
+					<div data-aos="fade-left" data-aos-delay="800" class="title">
+				                <h2><?= $title ?></h2>
+				    </div>
+				</div>
+			<?php } ?>
+
+			<?php if($embed_content) { ?>
+				<div class="cell small-12 large-6">
+					<div data-aos="fade-left" data-aos-delay="800" class="">
+				          <?= $embed_content ?>
+				    </div>
+				</div>
+			<?php } ?>
+
+	        <div class="cell large-12">
+		        <div class="embed-container">
+	            <?php echo get_sub_field('embed_code'); ?>
+		        </div>
 	        </div>
-        </div>
-    </div>
-</div>
+
+	</div>
+</section>
--- template-parts/full_width_text/block.php
+++ template-parts/full_width_text/block.php
@@ -7,14 +7,22 @@
 <section class="page-section main-container full-width-text" id="<?= $section_id; ?>" style="background-color: <?=$background;?>">
     <div class="grid-x grid-margin-x">
         <div class="cell small-12">
+
+	        <?php if($title) { ?>
             <div class="title">
                 <h2 data-aos="fade-left" data-aos-delay="750"><?= $title ?></h2>
             </div>
+            <?php } ?>
+
+            <?php if($content) { ?>
             <div data-aos="fade-right" data-aos-delay="1500">
 	            <div class="copy-box">
 	            <?= $content ?>
 	            </div>
 	        </div>
+
+	        <?php } ?>
+
         </div>
     </div>
 </section>
