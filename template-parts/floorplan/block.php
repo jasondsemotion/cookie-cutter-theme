b4e8e7db87bfabdcc20010d981709cf725de8336
Jason <jason@dsemotion.com>
2019-04-08 10:00:49 +0100

    Options added to floorplans

--- template-parts/floorplan/block.php
+++ template-parts/floorplan/block.php
@@ -4,13 +4,25 @@ $total_sq = get_sub_field('total_sq_ft');
 $total_m = get_sub_field('total_sq_m');
 $section_id = get_sub_field('section_id');
 $cont = get_sub_field('content');
+$layout = get_sub_field('heading_position');
 ?>
-<section class="page-section main-container gallery-container" id="<?= $section_id; ?>">
+<section class="page-section main-container gallery-container plan-container" id="<?= $section_id; ?>">
     <div class="grid-x grid-margin-x">
-        <div class="cell small-12 medium-7">
-            <div class="title title-center">
-                <h2 data-aos="fade-left" data-aos-delay="600">Floorplan</h2>
-            </div>
+        <div class="cell small-12 tablet-7 large-7">
+	        <?php if($layout == 'title-center') { ?>
+
+		        <div class="title title-center">
+	                <h2 data-aos="fade-left" data-aos-delay="600">Floorplan</h2>
+	            </div>
+
+	        <?php } else { ?>
+
+		        <div class="title">
+	                <h2 data-aos="fade-left" data-aos-delay="600">Floorplan</h2>
+	            </div>
+
+	        <?php } ?>
+
             <div data-aos="fade-left" data-aos-delay="900" class="owl-carousel owl-floorplan owl-theme">
                 <?php
                     $slide_counter = 0;
@@ -40,10 +52,23 @@ $cont = get_sub_field('content');
             <?php endwhile; else : endif; ?>
             </div>
         </div>
-        <div class="cell small-12 medium-5">
-            <div class="title" data-aos="fade-left" data-aos-delay="900">
-                <h2>Availability</h2>
-            </div>
+        <div class="cell small-12 tablet-5 large-5">
+
+	        <?php if($layout == 'title-center') { ?>
+
+		        <div class="title title-center">
+	                <h2 data-aos="fade-left" data-aos-delay="600">Availability</h2>
+	            </div>
+
+	        <?php } else { ?>
+
+		        <div class="title">
+	                <h2 data-aos="fade-left" data-aos-delay="600">Availability</h2>
+	            </div>
+
+	        <?php } ?>
+
+
             <!--<div class="title" data-aos="fade-left" data-aos-delay="1100">
                 <h4>Flexibility</h4>
             </div>
@@ -75,9 +100,49 @@ $cont = get_sub_field('content');
                     <td><strong><?= $total_m ?></strong></td>
                 </tr>
             </table>
-            <?php if($brochure) { ?>
-                <a href="<?= $brochure; ?>" class="button download-brochure-button">Download Brochure</a>
-            <?php } ?>
+
+
+
+
+
+			<?php if( have_rows('extra_buttons') ): ?>
+			<div class="extra-files">
+				<div class="grid-x grid-margin-x">
+
+
+					<?php if($brochure) { ?>
+					<div class="cell small-12 medium-12 large-12">
+						<div class="grid-x grid-margin-x">
+								<div class="cell small-12 medium-12 large-6">
+					                <a href="<?= $brochure; ?>" class="button download-brochure-button">Download</a>
+					            </div>
+						</div>
+					</div>
+				     <?php } ?>
+
+
+					<?php while( have_rows('extra_buttons') ): the_row();
+
+						// vars
+						$downloadcontent = get_sub_field('download_file');
+						$downloadtitle = get_sub_field('download_title');
+						?>
+
+						 <div class="cell small-12 medium-12 large-6">
+
+								<a href="<?php echo $downloadcontent; ?>" title="<?php echo $downloadcontent; ?>" class="button download-brochure-button">
+									<?php echo $downloadtitle; ?>
+								</a>
+
+						</div>
+
+					<?php endwhile; ?>
+
+				</div>
+			</div>
+			<?php endif; ?>
+
+
         </div>
     </div>
 </section>
